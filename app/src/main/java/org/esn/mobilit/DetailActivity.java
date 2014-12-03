52240ac852828bfb941e75f9d391f005af997eb5
JeremieSamson <jeremie.samson76@gmail.com>
2014-12-03 17:13:37 +0100

    Sources Changes

---
+++ app/src/main/java/org/esn/mobilit/DetailActivity.java
@@ -0,0 +1,53 @@
+package org.esn.mobilit;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebSettings.LayoutAlgorithm;
+import android.webkit.WebSettings.PluginState;
+import android.webkit.WebView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.esn.mobilit.parser.RSSFeed;
+
+public class DetailActivity extends Activity {
+
+	RSSFeed feed;
+	TextView title,link;
+	WebView desc;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.detail);
+
+		// Enable the vertical fading edge (by default it is disabled)
+		ScrollView sv = (ScrollView) findViewById(R.id.sv);
+		sv.setVerticalFadingEdgeEnabled(true);
+
+		// Get the feed object and the position from the Intent
+		feed = (RSSFeed) getIntent().getExtras().get("feed");
+		int pos = getIntent().getExtras().getInt("pos");
+
+		// Initialize the views
+		title = (TextView) findViewById(R.id.title);
+        link = (TextView) findViewById(R.id.linkDetail);
+		desc = (WebView) findViewById(R.id.desc);
+
+		// set webview properties
+		WebSettings ws = desc.getSettings();
+		ws.setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
+		ws.getPluginState();
+		ws.setPluginState(PluginState.ON);
+		ws.setJavaScriptEnabled(true);
+		ws.setBuiltInZoomControls(true);
+
+		// Set the views
+		title.setText(feed.getItem(pos).getTitle());
+        link.setText(feed.getItem(pos).getLink());
+		desc.loadDataWithBaseURL("http://www.androidcentral.com/", feed
+				.getItem(pos).getDescription(), "text/html", "UTF-8", null);
+	}
+
+}
---
+++ app/src/main/java/org/esn/mobilit/SplashActivity.java
@@ -0,0 +1,87 @@
+package org.esn.mobilit;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import org.esn.mobilit.parser.DOMParser;
+import org.esn.mobilit.parser.RSSFeed;
+
+/*
+ * Links
+ * http://www.androidbegin.com/tutorial/android-populating-spinner-json-tutorial/
+ */
+public class SplashActivity extends Activity {
+    private static final String TAG = SplashActivity.class.getSimpleName();
+	private String RSSFEEDURL = "http://esnlille.fr/events/feed";
+	RSSFeed feed;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+
+		ConnectivityManager conMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (conMgr.getActiveNetworkInfo() == null
+				&& !conMgr.getActiveNetworkInfo().isConnected()
+				&& !conMgr.getActiveNetworkInfo().isAvailable()) {
+			// No connectivity - Show alert
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage(
+					"Unable to reach server, \nPlease check your connectivity.")
+					.setTitle("ESN Mobil IT")
+					.setCancelable(false)
+					.setPositiveButton("Exit",
+							new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog,
+										int id) {
+									finish();
+								}
+							});
+
+			AlertDialog alert = builder.create();
+			alert.show();
+
+		} else {
+			// Connected - Start parsing
+			new AsyncLoadXMLFeed().execute();
+
+		}
+
+	}
+
+	private class AsyncLoadXMLFeed extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			// Obtain feed
+			DOMParser myParser = new DOMParser();
+			feed = myParser.parseXml(RSSFEEDURL);
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+
+			Bundle bundle = new Bundle();
+			bundle.putSerializable("feed", feed);
+
+			// launch List activity
+			Intent intent = new Intent(SplashActivity.this, ListActivity.class);
+			intent.putExtras(bundle);
+			startActivity(intent);
+
+			// kill this activity
+			finish();
+		}
+
+	}
+
+}
